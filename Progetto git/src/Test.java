6d2c76a041cd7e83fe4792e6a9ee775b5a1f756a
ilari <ilari@LAPTOP-5TGPM176>
2021-01-27 16:17:11 +0100

    Funzione di somma aggiunta

--- Progetto git/src/Test.java
+++ Progetto git/src/Test.java
@@ -4,6 +4,16 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ciao Anna");
+		
+		int numero = 0;
+		int somma = 0;
+		
+		somma = somma + numero;
+		
+		
+		
+		
+		
 
 	}
 
